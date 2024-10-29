d40d24f0caa71e938c1d3be4e5a15439f7c96968
AKorpi0 <antti@kkorpi.com>
2024-10-29 18:13:46 +0200

    US5 Test 4 W/GAI4

--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -78,3 +78,8 @@ class TestSpreadSheet(TestCase):
         ss = SpreadSheet()
         ss.set("A1", "=1/0")
         self.assertEqual("#Error", ss.evaluate("A1"))
+
+    def test_evaluate_formula_operators_multiply(self):
+        ss = SpreadSheet()
+        ss.set("A1", "=1+3*2")
+        self.assertEqual(7, ss.evaluate("A1"))
